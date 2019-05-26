480a25cbc8b45e7b579eaef7ec49d8903ab19933
AlanWu <15768716486@163.com>
2019-05-27 00:40:39 +0800

    页面布局中。。。。

--- src/taskDetails.js
+++ src/taskDetails.js
@@ -1,7 +1,11 @@
 import React from "react";
-import { Input, Button, Alert, message } from "antd";
+import { Input, Button, Alert, message, Card, Col, Row } from "antd";
 import jenkinsContext from "./my/util/JenkinsContext";
-
+import "./taskDetails.css";
+const gridStyle = {
+  width: "25%",
+  textAlign: "center"
+};
 export default class TaskDetails extends React.Component {
   componentDidMount() {
     console.log(jenkinsContext.isLogin);
@@ -10,57 +14,97 @@ export default class TaskDetails extends React.Component {
   render() {
     return (
       <div className="wrapper">
-        {/* 顶部登录模块 */}
-        <div>
-          <div
-            className={[
-              "Homeuser",
-              false === jenkinsContext.isLogin ? null : "noDisplay"
-            ].join(" ")}
-          >
-            <div className="name">
-              <Input
-                type="text"
-                size="large"
-                placeholder="id"
-                onChange={e => {
-                  console.log(e.target.value);
-                  jenkinsContext.userId = e.target.value;
-                  // this.setState({
-                  //   userId:e.target.value
-                  // })
-                }}
-              />
+        <div className="header">
+          {/* 顶部登录模块 */}
+          <div className="userLogin">
+            <div
+              className={[
+                "Homeuser",
+                false === jenkinsContext.isLogin ? null : "noDisplay"
+              ].join(" ")}
+            >
+              <div className="name">
+                <Input
+                  type="text"
+                  size="large"
+                  placeholder="id"
+                  onChange={e => {
+                    console.log(e.target.value);
+                    jenkinsContext.userId = e.target.value;
+                    // this.setState({
+                    //   userId:e.target.value
+                    // })
+                  }}
+                />
+              </div>
+              <div className="homeId">
+                <Button
+                  type="primary"
+                  size="large"
+                  onClick={() => {
+                    this.setState({});
+                  }}
+                >
+                  登录
+                </Button>
+              </div>
             </div>
-            <div className="homeId">
-              <Button
-                type="primary"
-                size="large"
-                onClick={() => {
-                  this.setState({});
-                }}
-              >
-                登录
+            <div
+              className={[
+                "Homeuser",
+                true === jenkinsContext.isLogin ? null : "noDisplay"
+              ].join(" ")}
+            >
+              <div id="loginState">
+                <Alert
+                  message={jenkinsContext.userId}
+                  description={jenkinsContext.IPaddress}
+                  type="success"
+                  className="Alert"
+                />
+              </div>
+            </div>
+          </div>
+          {/* 信息栏 */}
+          <div className="middle">
+            <div className="jobName">
+              <Alert message="jobName" type="success" />
+            </div>
+            <div className="description">
+              <Alert message="description" type="success" />
+            </div>
+            <div className="isRunning">
+              <Alert message="isRunning" type="success" />
+            </div>
+          </div>
+          {/* 是否运行 */}
+          <div className="right">
+            <div className="run">
+              <Button type="primary" size="large">
+                是否正在运行
+              </Button>
+            </div>
+            <div className="isState">
+              <Button type="primary" size="large">
+                Disable/Enable
               </Button>
             </div>
           </div>
-          <div
-            className={[
-              "Homeuser",
-              true === jenkinsContext.isLogin ? null : "noDisplay"
-            ].join(" ")}
-          >
-            <div id="loginState">
-              <Alert
-                message={jenkinsContext.userId}
-                description={jenkinsContext.IPaddress}
-                type="success"
-                className="Alert"
-              />
-            </div>
-          </div>
         </div>
-        {/* 信息栏 */}
+        <div className="body">
+          <div className="left">
+            <Card title="Card Title" noHovering>
+              <Card.Grid style={gridStyle}>Content</Card.Grid>
+              <Card.Grid style={gridStyle}>Content</Card.Grid>
+              <Card.Grid style={gridStyle}>Content</Card.Grid>
+              <Card.Grid style={gridStyle}>Content</Card.Grid>
+              <Card.Grid style={gridStyle}>Content</Card.Grid>
+              <Card.Grid style={gridStyle}>Content</Card.Grid>
+              <Card.Grid style={gridStyle}>Content</Card.Grid>
+            </Card>
+          </div>
+          <div className="right" />
+        </div>
       </div>
     );
   }
